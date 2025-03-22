65f1b1e3dd6df1c7655ab1f661cf8967da5317de
garafly <sara@puntodecontacto.net>
2025-03-22 08:59:40 -0400

    moved reset button

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -36,7 +36,7 @@ type BoardProps = {
   
         <button
           onClick={onResetScores}
-          className="mt-6 px-4 py-2 mt-10 bg-red-500 text-white rounded hover:bg-red-600 transition"
+          className="mt-6 px-4 py-2 mt-5 bg-red-500 text-white rounded hover:bg-red-600 transition"
         >
           Reset Scores
         </button>
